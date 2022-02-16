5c44054860e92106d3d3553a94bf6b64cdcfb4c1
loicbdev <loicbodolec@msn.com>
2022-02-16 15:36:53 +0100

    feat: camera

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,6 +1,40 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function CameraScreen() {
-  return <Text>Camera Screen</Text>;
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <>
+      <Camera style={styles.camera} ref={cameraRef} />
+      <Button
+        title="Take a picture"
+        onPress={async () => {
+          const pictureMetadata = await cameraRef.current.takePictureAsync();
+          console.log('pictureMetadata', pictureMetadata);
+        }}
+      />
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1
+  }
+});
