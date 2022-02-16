57014a3d580ff6112119bd1bacc8b5fab2f50220
loicbdev <loicbodolec@msn.com>
2022-02-16 15:22:21 +0100

    update: Ionicons

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import CameraScreen from './screens/CameraScreen';
 import FeedScreen from './screens/FeedScreen';
 import ImagesScreen from './screens/ImagesScreen';
@@ -10,8 +11,25 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Camera" component={CameraScreen} options={{ unmountOnBlur: true }} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Camera') {
+              iconName = focused ? 'camera' : 'camera-outline';
+            } else if (route.name === 'Images') {
+              iconName = focused ? 'image' : 'image-outline';
+            } else if (route.name === 'Feed') {
+              iconName = focused ? 'share-social' : 'share-social-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'gray'
+        })}
+      >
+        <Tab.Screen name="Camera" component={CameraScreen} />
         <Tab.Screen name="Images" component={ImagesScreen} />
         <Tab.Screen name="Feed" component={FeedScreen} />
       </Tab.Navigator>
