013ab3a2494929ae13d7dcbc4e6450c5205c88b0
loicbdev <loicbodolec@msn.com>
2022-02-16 15:17:29 +0100

    feat: navigation

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CameraScreen from './screens/CameraScreen';
+import FeedScreen from './screens/FeedScreen';
+import ImagesScreen from './screens/ImagesScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Camera" component={CameraScreen} />
+        <Tab.Screen name="Images" component={ImagesScreen} />
+        <Tab.Screen name="Feed" component={FeedScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
