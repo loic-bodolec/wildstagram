21d7c7b737e84ea9cf66ffebe40cb281ac7ca1fa
loicbdev <loicbodolec@msn.com>
2022-02-16 16:09:09 +0100

    feat: photos gallery

--- screens/ImagesScreen.js
+++ screens/ImagesScreen.js
@@ -1,6 +1,40 @@
-import React from 'react';
-import { Text } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image, FlatList } from 'react-native';
 
 export default function ImagesScreen() {
-  return <Text>Images Screen</Text>;
+  const [imagesURI, setImagesURI] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const images = await FileSystem.readDirectoryAsync(FileSystem.cacheDirectory + 'ImageManipulator');
+      setImagesURI(images);
+    })();
+  }, []);
+  return imagesURI.length > 0 ? (
+    <FlatList
+      data={imagesURI}
+      keyExtractor={(imageURI) => imageURI}
+      renderItem={(itemData) => {
+        console.log('item', itemData);
+        return (
+          <Image
+            style={styles.image}
+            source={{
+              uri: FileSystem.cacheDirectory + 'ImageManipulator/' + itemData.item
+            }}
+          />
+        );
+      }}
+    />
+  ) : null;
 }
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: 'cover',
+    height: 400,
+    margin: 20,
+    borderColor: 'grey',
+    borderWidth: 2
+  }
+});
