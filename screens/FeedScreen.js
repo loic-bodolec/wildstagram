f4f3017aeedba6cede71217b9a6c27c32651e8f8
loicbdev <loicbodolec@msn.com>
2022-02-16 16:37:18 +0100

    feat: upload photos

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,6 +1,40 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Image, FlatList, StyleSheet } from 'react-native';
 
 export default function FeedScreen() {
-  return <Text>Feed Screen</Text>;
+  const [serverImagesUrls, setServerImagesUrls] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const filesUrl = await axios.get('https://wildstagram.nausicaa.wilders.dev/list');
+      console.log('filesurls', filesUrl.data);
+      setServerImagesUrls(filesUrl.data);
+    })();
+  }, []);
+  return serverImagesUrls.length > 0 ? (
+    <FlatList
+      data={serverImagesUrls}
+      keyExtractor={(serverImageURI) => serverImageURI}
+      renderItem={(itemData) => {
+        console.log('item', itemData);
+        return (
+          <>
+            <Image
+              style={styles.image}
+              source={{
+                uri: 'https://wildstagram.nausicaa.wilders.dev/files/' + itemData.item
+              }}
+            />
+          </>
+        );
+      }}
+    />
+  ) : null;
 }
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: 'contain',
+    height: 500
+  }
+});
--- screens/ImagesScreen.js
+++ screens/ImagesScreen.js
@@ -1,6 +1,7 @@
 import * as FileSystem from 'expo-file-system';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, FlatList } from 'react-native';
+import { StyleSheet, Image, FlatList, Button } from 'react-native';
+import singleFileUploader from 'single-file-uploader';
 
 export default function ImagesScreen() {
   const [imagesURI, setImagesURI] = useState([]);
@@ -17,12 +18,31 @@ export default function ImagesScreen() {
       renderItem={(itemData) => {
         console.log('item', itemData);
         return (
-          <Image
-            style={styles.image}
-            source={{
-              uri: FileSystem.cacheDirectory + 'ImageManipulator/' + itemData.item
-            }}
-          />
+          <>
+            <Image
+              style={styles.image}
+              source={{
+                uri: FileSystem.cacheDirectory + 'ImageManipulator/' + itemData.item
+              }}
+            />
+            <Button
+              title="upload"
+              onPress={async () => {
+                try {
+                  await singleFileUploader({
+                    distantUrl: 'https://wildstagram.nausicaa.wilders.dev/upload',
+                    filename: itemData.item,
+                    filetype: 'image/jpeg',
+                    formDataName: 'fileData',
+                    localUri: FileSystem.cacheDirectory + 'ImageManipulator/' + itemData.item
+                  });
+                  alert('Uploaded');
+                } catch (err) {
+                  alert('Error');
+                }
+              }}
+            />
+          </>
         );
       }}
     />
